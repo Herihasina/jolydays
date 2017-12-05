c563fdef09f074921c71d9a4511469faae7e0955
Hery <herihasina.rr@gmail.com>
2017-12-06 00:39:12 +0300

    Call Twig template from Symfony php file

--- src/JD/JolydaysBundle/Controller/PostsController.php
+++ src/JD/JolydaysBundle/Controller/PostsController.php
@@ -3,8 +3,6 @@
 namespace JD\JolydaysBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class PostsController extends Controller
@@ -12,48 +10,65 @@ class PostsController extends Controller
   // path /
   public function indexAction($page)
   {
-    // if valid page number, show next lists of posts
-    // else show error
-    if ($page >= 1) {
-      return new Response('All posts are here');
-    }else{
-      throw new NotFoundHttpException('Page '.$page.' does not exist');
-    } 
+    // sample datas for populating homepage
+    $all_posts = array(
+      array(
+        'title'   => 'First post',
+        'id'      => 1),
+      array(
+        'title'   => 'Another post',
+        'id'      => 2)
+    );    
+
+    // Call index.html.twig and send posts parameter
+    return $this->render('JDJolydaysBundle:Posts:index.html.twig', array(
+      'posts' => $all_posts
+    ));
   }
 
-  // path /post/$post_id_to_view
+  // path /post/$post_id, $post_id is from url
   public function viewAction($post_id)
   {
-    return new Response('Single page for the post with id ' . $post_id);
+    // sample datas for populating the single page
+    $a_post = array(
+      'title'   => 'Random post',
+      'id'      => $post_id,
+      'author'  => 'John Doe',
+      'content' => 'Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      'date'    => new \Datetime()
+    );
+
+    // Call view.html.twig and send post parameter
+    return $this->render('JDJolydaysBundle:Posts:view.html.twig', array(
+      'post' => $a_post
+    ));
   }
 
   // path /add
-  public function addAction(Request $request)
+  public function addAction()
   {
     // Assuming that datas were collected from forms
     if ($request->isMethod('POST')) {
       
       // TODO: save post to db (...) and get its id
-
-      return new Response('Post has been successfully added');
     }
 
-    // if /add url was manually inserted, show the add form
-    return new Response('Form for adding posts');
+    // if /add url was from link, show the add form
+    return $this->render('JDJolydaysBundle:Posts:add.html.twig');
   }
 
   // path /edit/$post_id_to_edit
-  public function editAction($post_id, Request $request)
+  public function editAction($post_id)
   {
     if ($request->isMethod('POST')) {
 
       // TODO: alter post to db (...) and get its id to view it
-
-      return new Response('The post with id '.$post_id.' has been successfully edited');
     }
 
-      // if /edit/$post_id url was manually inserted, show the edit form
-      return new Response('Form for editing posts');
+    // if /edit/{} url was from link, show the add form
+    return $this->render('JDJolydaysBundle:Posts:edit.html.twig');
       
   }
 
@@ -62,6 +77,6 @@ class PostsController extends Controller
   {
     // TODO: delete post with post_id id to db
     //show the page with a delete button
-    return new Response('Form with a delete button');
+    return $this->render('JDJolydaysBundle:Posts:delete.html.twig', array('post_id' => $post_id ));
   }
 }
